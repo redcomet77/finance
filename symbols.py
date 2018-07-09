fb4ddccde303e9b5178f04d574dcd09e40f67c99
vyee <vyee@micron.com>
2018-07-09 16:26:23 -0700

    add sap500 list

--- symbols.py
+++ symbols.py
@@ -3,6 +3,9 @@ import numpy as numpy
 import datetime
 import pandas_datareader
 import pandas_datareader.data as web
+import bs4 as bs
+import pickle
+import requests
 
 RED = '\033[91m'
 GREEN = '\033[92m'
@@ -21,6 +24,20 @@ class symbols(object):
         symData = web.DataReader(self.symStr, 'morningstar', start, end)
         return symData
 
+    def save_sp500_tickers(self):
+        resp = requests.get('http://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+        soup = bs.BeautifulSoup(resp.text, 'lxml')
+        table = soup.find('table', {'class': 'wikitable sortable'})
+        tickers = []
+        for row in table.findAll('tr')[1:]:
+            ticker = row.findAll('td')[0].text
+            tickers.append(ticker)
+
+        with open("sp500tickers.pickle","wb") as f:
+            pickle.dump(tickers,f)
+
+        return tickers
+
     def calcSignals(self, df_sod, df_sok, df_smi, df_sig, period):
         sodStr = 'SO%d_'+str(period)
         sokStr = 'SO%k'
@@ -28,8 +45,10 @@ class symbols(object):
         self.signal = df_sok[sokStr].subtract(df_sod[sodStr])
         self.smi = df_smi.subtract(df_sig)
         last = self.signal[0]
+        last2 = self.smi[0]
         days_since_last_flip = 0
-        last_toggle = False if last < 0 else True
+        last_toggle = False if last < 0 and last2 < 0 else True
+        sigStr = RED+'sell' if last < 0 and last2 < 0 else GREEN+'buy'
 
         for c, d in zip(self.signal, self.smi) :
             toggle = False if c < 0 and d < 0 else True
@@ -40,16 +59,18 @@ class symbols(object):
                 last_toggle = toggle
 
         if (self.signal[-1] < 0 and self.smi[-1] < 0):
-            self.printSig(RED+'sell', days_since_last_flip)
+            sigStr = RED+'sell'
+            self.printSig(sigStr, days_since_last_flip)
         elif (self.signal[-1] > 0 and self.smi[-1] > 0):
-            self.printSig(GREEN+'buy', days_since_last_flip)
+            sigStr = GREEN+'buy'
+            self.printSig(sigStr, days_since_last_flip)
         else:
-            self.printSig(YELLOW+'hold', days_since_last_flip)
+            self.printSig(sigStr, days_since_last_flip)
 
     def printSig(self, s, d):
-        t = GREEN+'buy' if s == RED+'sell' else YELLOW+'hold'
-        print("{0} {1} {2} \033[0m : days since last {3}: \033[0m {4}".format(self.signal.index.get_level_values(0)[0], \
+        t = GREEN+'buy' if s == RED+'sell' else RED+'sell'
+        print("{0} {1} {2} \033[0m : days since last {3}: \033[0m {4} \n {5} {6}".format(self.signal.index.get_level_values(0)[0], \
                                                                 self.signal.index.get_level_values(1)[-1], \
                                                                 s, \
                                                                 t, \
-                                                                d))
+                                                                d, self.smi[-1], self.signal[-1]))
